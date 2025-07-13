659dfe9d88db84c6fbcf66adc6d552fde455b3a9
unkreativimweb <vito.webskyx@gmail.com>
2025-07-13 15:10:35 +0200

    reworked id_to_element_name to be more effective

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from env import initialize_spotify_client
+from src.env import initialize_spotify_client
 
 global sp
 sp = initialize_spotify_client()  # Assuming this function initializes the Spotify client
@@ -17,55 +17,72 @@ def string_to_list(string):
     # print("Converted list: ", cleaned_items)
     return cleaned_items
 
-def id_to_element_name(element_id):
+def id_to_element_name(element_id, type=None, type_given=True):
     """
     Convert a Spotify ID to a readable name, handling different types (track, playlist, album, artist)
 
-    !! TODO: BE AWARE: the ouput for songs was changed to a dict and the rest will follow (this func needs overhaul anyway lol)
-
     Args:
         element_id (str): Spotify ID of the element
+        type (str): type of element searched for, can be None if not sure
+        type_given (bool): so program knows to go the long way round
     Returns:
         str: Formatted name of the element
+        dict: if type == track with artist and track name
     """
-    try:
-        # First try as playlist
-        try:
-            element = sp.playlist(element_id)
-            return f"{element['name']} (playlist)"
-        except:
-            pass
-        
-        # Then try as track
-        try:
-            element = sp.track(element_id)
-            return {
-                "track_name": element['name'],
-                "artist": {element['artists'][0]['name']} if element['artists'] else None
-            }
-        except:
-            pass
-        
-        # Then try as album
-        try:
-            element = sp.album(element_id)
-            return f"{element['name']} (album) - {element['artists'][0]['name']}"
-        except:
-            pass
-        
-        # Finally try as artist
-        try:
-            element = sp.artist(element_id)
-            return f"{element['name']} (artist)"
-        except:
-            pass
-            
-        raise Exception("Could not identify element type")
-        
-    except Exception as e:
-        print(f"Error getting element name: {e}")
-        return f"Unknown Element ({element_id})"
 
+    if type_given:
+        if type == "track":
+            return {
+                    "track_name": element['name'],
+                    "artist": {element['artists'][0]['name']} if element['artists'] else None
+            }
+        else:     
+            try:
+                element = getattr(sp, type)(element_id)
+                return element["name"]
+            except Exception as e:
+                print("Error finding element_name from id in utils.py")
+    
+    # vvv Technically old, unused vvv
+    else:
+        try:
+            # First try as playlist
+            try:
+                element = sp.playlist(element_id)
+                return {element['name']}
+            except:
+                pass
+            
+            # Then try as track
+            try:
+                element = sp.track(element_id)
+                return {
+                    "track_name": element['name'],
+                    "artist": {element['artists'][0]['name']} if element['artists'] else None
+                }
+            except:
+                pass
+            
+            # Then try as album
+            try:
+                element = sp.album(element_id)
+                return {element['name']}
+            except:
+                pass
+            
+            # Finally try as artist
+            try:
+                element = sp.artist(element_id)
+                return {element['name']}
+            except:
+                pass
+                
+            raise Exception("Could not identify element type")
+            
+        except Exception as e:
+            print(f"Error getting element name: {e}")
+            return f"Unknown Element ({element_id})"
+        
 def element_name_to_id(element_name, element_type):
     """
     Convert a readable name to a Spotify ID, handling different types (track, playlist, album, artist)
@@ -76,42 +93,9 @@ def element_name_to_id(element_name, element_type):
         str: Spotify ID of the element
     """
     try: 
-        item = sp.search(q=element_name, limit=10, offset=0, type=element_type, market=None)
-        return item['tracks']['items'][0]['id'] if element_type == 'track' else None
+        item = sp.search(q=element_name, type=element_type)
+        return item['artists']['items'][0]['id']
     except Exception as e:
         print(f"Error searching for element: {e}")
         return None
     
-    '''
-        try:
-            if element_type == 'playlist':
-                playlists = sp.current_user_playlists()
-                for item in playlists['items']:
-                    if item['name'] == element_name:
-                        return item['id']
-            
-            elif element_type == 'track':
-                results = sp.search(q='track:' + element_name, type='track', limit=1)
-                if results['tracks']['items']:
-                    return results['tracks']['items'][0]['id']
-            
-            elif element_type == 'album':
-                results = sp.search(q='album:' + element_name, type='album', limit=1)
-                if results['albums']['items']:
-                    return results['albums']['items'][0]['id']
-            
-            elif element_type == 'artist':
-                results = sp.search(q='artist:' + element_name, type='artist', limit=1)
-                if results['artists']['items']:
-                    return results['artists']['items'][0]['id']
-            
-            
-
-            raise Exception("Could not identify element type")
-            
-        except Exception as e:
-            print(f"Error converting name to ID: {e}")
-            return None
-
-    '''
-        
